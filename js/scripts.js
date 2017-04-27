af67b235ff35691aa67abf32cc7e782e56140af7
Ian Garner, Jenna Cooper and Roger Tobias <jenna@>
2017-04-27 11:24:20 -0700

    add everything unfortunely we had troubles and forgot to commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
-  var animal = (prompt("How old are you?"));
 
-  if (age >= 8) {
-    $('#drinks').show();
-  }
+
+  $("form#animals").submit(function(event) {
+    var animal = $("input:radio[name=animal]:checked").val();
+     if (animal === "ladybugs") {
+        $('.ladybugs').show();
+        $('.turtle').hide();
+        $('.spiders').hide();
+      } else if (animal === "turtle") {
+        $('.ladybugs').hide();
+        $('.turtle').show();
+        $('.spiders').hide();
+      } else {
+        $('.ladybugs').hide();
+        $('.turtle').hide();
+        $('.spiders').show();
+      }
+
+
+  event.preventDefault();
+  });
+
 });
-var animal = $("input:radio[name=animal]:checked").val();
