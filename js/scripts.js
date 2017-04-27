12c04b90cd0de4ec47c5e9f1e37eee2fe7db899d
Ian Garner, Jenna Cooper and Roger Tobias <jenna@>
2017-04-27 09:12:51 -0700

    add styling and html setup

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var animal = (prompt("How old are you?"));
+
+  if (age >= 8) {
+    $('#drinks').show();
+  }
+});
+var animal = $("input:radio[name=animal]:checked").val();
